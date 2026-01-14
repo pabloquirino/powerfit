8ebb33d8501039fd301fecd7fc979c2e628d8272
Pablo Quirino <pabloquirino17@gmail.com>
2026-01-14 15:59:47 -0300

    feat: add responsive benefits section with grid layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
 import { HeroComponent } from './components/hero/hero.component';
+import { BenefitsComponent } from './components/benefits/benefits.component';
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
-    HeroComponent
+    HeroComponent,
+    BenefitsComponent
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
--- src/app/components/benefits/benefits.component.ts
+++ src/app/components/benefits/benefits.component.ts
@@ -1,11 +1,35 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Card } from './model/model';
 
 @Component({
   selector: 'app-benefits',
-  imports: [],
+  standalone: true,
+  imports: [CommonModule],
   templateUrl: './benefits.component.html',
-  styleUrl: './benefits.component.scss'
+  styleUrls: ['./benefits.component.scss']
 })
 export class BenefitsComponent {
-
+  benefits: Card[] = [
+    {
+      image: 'assets/equipamentos-modernos.avif',
+      title: 'Equipamentos modernos',
+      description: 'Aparelhos novos e manutenção constante.'
+    },
+    {
+      image: 'assets/personal-trainer.avif',
+      title: 'Personal trainers',
+      description: 'Profissionais qualificados.'
+    },
+    {
+      image: 'assets/powerfit.jpg',
+      title: 'Horários flexíveis',
+      description: 'Aberto todos os dias.'
+    },
+    {
+      image: 'assets/treino.avif',
+      title: 'Treinos personalizados',
+      description: 'Planos feitos para você.'
+    }
+  ];
 }

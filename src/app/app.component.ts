b76c20cdd29b191e565e3c536cee9b1261dba768
Pablo Quirino <pabloquirino17@gmail.com>
2026-01-14 15:15:27 -0300

    feat: add responsive hero section with CTA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "./components/header/header.component";
+import { HeroComponent } from './components/hero/hero.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [
+    HeaderComponent,
+    HeroComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

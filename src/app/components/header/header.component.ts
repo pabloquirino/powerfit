17fa91f577babe8ea4af448ab2f8b02a972584bd
Pablo Quirino <pabloquirino17@gmail.com>
2026-01-14 14:46:38 -0300

    feat: responsive header with mobile menu

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,10 +2,13 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  imports: [],
   templateUrl: './header.component.html',
-  styleUrl: './header.component.scss'
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
+  isMenuOpen = false;
 
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
